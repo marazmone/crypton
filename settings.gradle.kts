36aff9495679e3adc060a46f75d1a744c59700af
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-06-08 17:23:13 +0200

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CryptoN"
+include(":androidApp")
+include(":shared")
