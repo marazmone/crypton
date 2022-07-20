71a4159e86829afd7ad8a6892157d49d9790df82
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-07-21 00:32:38 +0200

    [Android] add state component to list, favorite and detail screens

--- androidApp/src/main/java/com/marazmone/crypton/android/presentation/screen/currency/favorite/CurrencyFavoritePreview.kt
+++ androidApp/src/main/java/com/marazmone/crypton/android/presentation/screen/currency/favorite/CurrencyFavoritePreview.kt
@@ -6,9 +6,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.marazmone.crypton.android.R
 import com.marazmone.crypton.android.presentation.ui.AppTheme
 import com.marazmone.crypton.android.presentation.ui.component.currency.CurrencyItemComponent
+import com.marazmone.crypton.android.presentation.ui.component.state.ImageWithTextActionStateComponent
 import com.marazmone.crypton.android.presentation.util.PaddingValuesVertical
 import com.marazmone.crypton.domain.model.currency.CurrencyListItem
 
@@ -27,6 +30,44 @@ private fun SuccessStatePreview() {
                     CurrencyItemComponent(item = item)
                 }
             }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF11124B
+)
+@Composable
+private fun ErrorStatePreview() {
+    AppTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            ImageWithTextActionStateComponent(
+                resId = R.drawable.im_error_state,
+                text = stringResource(id = R.string.something_wrong)
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF11124B
+)
+@Composable
+private fun EmptyStatePreview() {
+    AppTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            ImageWithTextActionStateComponent(
+                resId = R.drawable.im_empty_state,
+                text = stringResource(id = R.string.favorite_empty_state)
+            )
         }
     }
 }
--- androidApp/src/main/java/com/marazmone/crypton/android/presentation/screen/currency/list/CurrencyListPreview.kt
+++ androidApp/src/main/java/com/marazmone/crypton/android/presentation/screen/currency/list/CurrencyListPreview.kt
@@ -1,19 +1,19 @@
 package com.marazmone.crypton.android.presentation.screen.currency.list
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.marazmone.crypton.android.R
 import com.marazmone.crypton.android.presentation.ui.AppTheme
-import com.marazmone.crypton.android.presentation.ui.Colors.Main.Background
 import com.marazmone.crypton.android.presentation.ui.component.currency.CurrencyItemComponent
+import com.marazmone.crypton.android.presentation.ui.component.state.ImageWithTextActionStateComponent
 import com.marazmone.crypton.android.presentation.util.PaddingValuesVertical
 import com.marazmone.crypton.domain.model.currency.CurrencyListItem
 
@@ -26,42 +26,53 @@ private fun SuccessStatePreview() {
                 .fillMaxSize(),
         ) {
             LazyColumn(
-                contentPadding = PaddingValuesVertical()
+                contentPadding = PaddingValuesVertical(),
             ) {
                 items(CurrencyListItem.emptyList().sortedBy { it.rank }) { item ->
-                    CurrencyItemComponent(item = item)
+                    CurrencyItemComponent(
+                        item = item,
+                    )
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF11124B,
+)
 @Composable
 private fun LoadingStatePreview() {
     AppTheme {
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Background),
-            contentAlignment = Alignment.Center,
+                .fillMaxSize(),
         ) {
-            CircularProgressIndicator()
+            Box(
+                contentAlignment = Alignment.Center,
+            ) {
+                CircularProgressIndicator()
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF11124B,
+)
 @Composable
 private fun ErrorStatePreview() {
     AppTheme {
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .background(Background),
-            contentAlignment = Alignment.Center,
+                .fillMaxSize(),
         ) {
-            Text(text = "Error: test error")
+            ImageWithTextActionStateComponent(
+                resId = R.drawable.im_error_state,
+                text = stringResource(id = R.string.something_wrong),
+            )
         }
     }
 }
