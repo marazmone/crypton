b074dd78d3567bfb797ae9073cbefe86df9c0e18
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-12-12 17:12:13 +0100

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.22")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.7.20")
         classpath("io.realm.kotlin:gradle-plugin:1.5.1")
     }
 }
