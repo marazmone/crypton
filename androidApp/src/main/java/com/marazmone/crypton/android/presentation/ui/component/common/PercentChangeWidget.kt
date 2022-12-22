ef8c1c2ac07c74cd7d05fdc744228a60c2231536
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-12-22 22:02:42 +0100

    - resolve lints

--- androidApp/src/main/java/com/marazmone/crypton/android/presentation/ui/component/common/PercentChangeWidget.kt
+++ androidApp/src/main/java/com/marazmone/crypton/android/presentation/ui/component/common/PercentChangeWidget.kt
@@ -1,8 +1,6 @@
 package com.marazmone.crypton.android.presentation.ui.component.common
 
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
@@ -15,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
--- androidApp/src/main/java/com/marazmone/crypton/android/presentation/ui/component/common/PullRefreshWidget.kt
+++ androidApp/src/main/java/com/marazmone/crypton/android/presentation/ui/component/common/PullRefreshWidget.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.marazmone.crypton.android.presentation.ui.AppTheme
 import com.marazmone.crypton.android.presentation.ui.Colors.Main
 
